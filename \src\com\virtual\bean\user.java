87cd40351597d85a377101324f5662a594ee7be0
Feurial Black <rexchester@rediffmail.com>
2020-11-19 18:33:06 +0530

    Create \src\com\virtual\bean\user.java

---
+++ \src\com\virtual\bean\user.java
@@ -0,0 +1,49 @@
+package com.virtual.bean;
+
+
+public class User {
+	int userId;
+    String fname,lname,email,password,contact,address;
+	public int getUserId() {
+		return userId;
+	}
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	public String getFname() {
+		return fname;
+	}
+	public void setFname(String fname) {
+		this.fname = fname;
+	}
+	public String getLname() {
+		return lname;
+	}
+	public void setLname(String lname) {
+		this.lname = lname;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getContact() {
+		return contact;
+	}
+	public void setContact(String contact) {
+		this.contact = contact;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	} 
+}
